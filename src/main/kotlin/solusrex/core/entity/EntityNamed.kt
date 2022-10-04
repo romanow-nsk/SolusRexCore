8d34a418f6d4e722061fea04b016904c77fe8f7d
romanow-nsk <romanow@ngs.ru>
2022-10-04 22:26:46 +0700

    Fix0

--- src/main/kotlin/solusrex/core/entity/EntityNamed.kt
+++ src/main/kotlin/solusrex/core/entity/EntityNamed.kt
@@ -1,8 +1,5 @@
 package solusrex.core.entity
 
 class EntityNamed : Entity() {
-    override fun title() = ""
-    override fun toString(): String {
-        return "$oid ${title()}"
+    var name = ""
     }
-}
